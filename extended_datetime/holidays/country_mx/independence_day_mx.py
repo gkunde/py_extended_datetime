9c58a73f552b384aeb25def1c50bb4e9609720fb
G. Kunde <34901900+gkunde@users.noreply.github.com>
2026-02-06 16:19:08 -0600

    Implement Independence Day check for Mexico

--- extended_datetime/holidays/country_mx/independence_day_mx.py
+++ extended_datetime/holidays/country_mx/independence_day_mx.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 
+from extended_datetime.base.holiday_base import HolidayBase
+
+
+class IndependenceDayMX(HolidayBase):
+
+    def check_date(self, date_to_check: datetime) -> bool:
+
+        if date_to_check.month == 9 and date_to_check.day == 16:
+            return True
+
+        return False
