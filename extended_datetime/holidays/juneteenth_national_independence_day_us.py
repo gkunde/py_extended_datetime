2db02ce9eee7bda53bd16ed8ee94d5df56c249d4
G. Kunde <34901900+gkunde@users.noreply.github.com>
2026-02-05 21:38:08 -0600

    Add remaining US Federal Holidays (#13)
    * Add Memorial Day US
    
    * Add JuneteenthNationalIndependenceDayUS
    
    * Fix month check for Juneteenth holiday
    
    * Add Labor Day US
    
    * Add Thanksgiving Day US
    
    * Add ChristmasDayUS
    
    * Add VeteransDayUS
    
    * BirthdayMartinLutherKingJrUS
    
    * Add Columbus Day US

---
+++ extended_datetime/holidays/juneteenth_national_independence_day_us.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from extended_datetime.base.holiday_base import HolidayBase
+
+
+class JuneteenthNationalIndependenceDayUS(HolidayBase):
+
+    def check_date(self, date_to_check: datetime) -> bool:
+
+        if date_to_check.month != 6:
+            return False
+
+        if date_to_check.day == 19:
+            return True
+
+        if date_to_check.day == 18 and date_to_check.isoweekday() == 5:
+            return True
+
+        if date_to_check.day == 20 and date_to_check.isoweekday() == 1:
+            return True
+
+        return False
