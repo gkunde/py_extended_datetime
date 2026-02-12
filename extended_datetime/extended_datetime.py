b5784a3b44586c2811b3fa6f532ddda64f5da4c4
G. Kunde <34901900+gkunde@users.noreply.github.com>
2026-02-11 19:07:00 -0600

    Use a math type approach to finding the end of the month. (#22)
    * Use a math type approach to finding the end of the month.
    
    * Add comments

--- extended_datetime/extended_datetime.py
+++ extended_datetime/extended_datetime.py
@@ -154,22 +154,17 @@ class ExtendedDateTime(datetime):
 
         if not month:
             month = self.month
+        
+        # move to the first day of the given month
+        _start_of_month = datetime(year, month, 1)
+        # add 32 days to ensure we are in the next month
+        _start_of_month += timedelta(days=32)
+        # move back to the first day of "current" month
+        _start_of_month = _start_of_month.replace(day=1)
 
-        end_of_month = 31
+        # subtract one day to get the last day of the month and return the day value
+        return (_start_of_month - timedelta(days=1)).day
 
-        if month in (4, 6, 9, 11, ):
-            # Short months
-            end_of_month = 30
-
-        elif month == 2:
-            # February
-            end_of_month = 28
-
-            if self.is_leap_year(year):
-                # Leap Year
-                end_of_month = 29
-
-        return end_of_month
 
     def is_leap_year(self, year: Optional[int] = None) -> bool:
         """
