cc04a1a74406cd72ecd15b6fbd2a626950ce8d6e
G. Kunde <34901900+gkunde@users.noreply.github.com>
2026-02-19 18:03:31 -0600

    Use builtins (#27)
    * Use built-in libraries that compute desired values.
    
    * update test cases to use new methods

--- tests/test_extended_datetime.py
+++ tests/test_extended_datetime.py
@@ -13,43 +13,43 @@ class Test_ExtendedDateTime(unittest.TestCase):
 
         o = ExtendedDateTime(2000, 1, 1)
 
-        self.assertEqual(o.date_add(years=1), datetime(2001, 1, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(years=-1), datetime(1999, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_years(1), datetime(2001, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_years(-1), datetime(1999, 1, 1, 0, 0, 0))
 
     def test_date_add_months(self):
 
         o = ExtendedDateTime(2000, 1, 1)
 
-        self.assertEqual(o.date_add(months=1), datetime(2000, 2, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(1), datetime(2000, 2, 1, 0, 0, 0))
 
     def test_date_add_months_from_base(self):
 
         o = ExtendedDateTime(2000, 1, 31)
 
-        self.assertEqual(o.date_add(months=1), datetime(2000, 2, 29, 0, 0, 0))
-        self.assertEqual(o.date_add(months=2), datetime(2000, 3, 31, 0, 0, 0))
-        self.assertEqual(o.date_add(months=3), datetime(2000, 4, 30, 0, 0, 0))
-        self.assertEqual(o.date_add(months=6), datetime(2000, 7, 31, 0, 0, 0))
-        self.assertEqual(o.date_add(months=11), datetime(2000, 12, 31, 0, 0, 0))
+        self.assertEqual(o.add_months(1), datetime(2000, 2, 29, 0, 0, 0))
+        self.assertEqual(o.add_months(2), datetime(2000, 3, 31, 0, 0, 0))
+        self.assertEqual(o.add_months(3), datetime(2000, 4, 30, 0, 0, 0))
+        self.assertEqual(o.add_months(6), datetime(2000, 7, 31, 0, 0, 0))
+        self.assertEqual(o.add_months(11), datetime(2000, 12, 31, 0, 0, 0))
 
-        self.assertEqual(o.date_add(months=-1), datetime(1999, 12, 31, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-2), datetime(1999, 11, 30, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-6), datetime(1999, 7, 31, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-11), datetime(1999, 2, 28, 0, 0, 0))
+        self.assertEqual(o.add_months(-1), datetime(1999, 12, 31, 0, 0, 0))
+        self.assertEqual(o.add_months(-2), datetime(1999, 11, 30, 0, 0, 0))
+        self.assertEqual(o.add_months(-6), datetime(1999, 7, 31, 0, 0, 0))
+        self.assertEqual(o.add_months(-11), datetime(1999, 2, 28, 0, 0, 0))
     
     def test_date_add_months_long_range(self):
 
         o = ExtendedDateTime(2000, 1, 1, 0, 0 ,0)
 
-        self.assertEqual(o.date_add(months=12), datetime(2001, 1, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=13), datetime(2001, 2, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=15), datetime(2001, 4, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=24), datetime(2002, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(12), datetime(2001, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(13), datetime(2001, 2, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(15), datetime(2001, 4, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(24), datetime(2002, 1, 1, 0, 0, 0))
 
-        self.assertEqual(o.date_add(months=-12), datetime(1999, 1, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-13), datetime(1998, 12, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-15), datetime(1998, 10, 1, 0, 0, 0))
-        self.assertEqual(o.date_add(months=-24), datetime(1998, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(-12), datetime(1999, 1, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(-13), datetime(1998, 12, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(-15), datetime(1998, 10, 1, 0, 0, 0))
+        self.assertEqual(o.add_months(-24), datetime(1998, 1, 1, 0, 0, 0))
 
     def test_date_add_months_chained(self):
 
@@ -58,31 +58,31 @@ class Test_ExtendedDateTime(unittest.TestCase):
         o = ExtendedDateTime(2000, 1, 31)
 
         # This will arrive at the 29th since the 2nd month of the year does not have 31 days
-        n = o.date_add(months=1)
+        n = o.add_months(1)
         self.assertEqual(n, datetime(2000, 2, 29, 0, 0, 0))
 
         # This will carry the 29th since the 3rd month of the year does have at least 29 days.
-        n = n.date_add(months=1)
+        n = n.add_months(1)
         self.assertEqual(n, datetime(2000, 3, 29, 0, 0, 0))
 
     def test_date_add_months_and_years(self):
 
         o = ExtendedDateTime(2000, 1, 1)
 
-        self.assertEqual(o.date_add(years=1, months=1), datetime(2001, 2, 1))
-        self.assertEqual(o.date_add(years=1, months=-1), datetime(2000, 12, 1))
+        self.assertEqual(o.add_years(1).add_months(1), datetime(2001, 2, 1))
+        self.assertEqual(o.add_years(1).add_months(-1), datetime(2000, 12, 1))
 
-        self.assertEqual(o.date_add(years=-1, months=14), datetime(2000, 3, 1))
-        self.assertEqual(o.date_add(years=1, months=-14), datetime(1999, 11, 1))
+        self.assertEqual(o.add_years(-1).add_months(14), datetime(2000, 3, 1))
+        self.assertEqual(o.add_years(1).add_months(-14), datetime(1999, 11, 1))
 
-        self.assertEqual(o.date_add(years=2, months=14), datetime(2003, 3, 1))
-        self.assertEqual(o.date_add(years=-2, months=-14), datetime(1996, 11, 1))
+        self.assertEqual(o.add_years(2).add_months(14), datetime(2003, 3, 1))
+        self.assertEqual(o.add_years(-2).add_months(-14), datetime(1996, 11, 1))
 
     def test_date_add_days(self):
 
         o = ExtendedDateTime(2000, 1, 1)
 
-        self.assertEqual(o.date_add(days=1), datetime(2000, 1, 2))
+        self.assertEqual(o.add_days(1), datetime(2000, 1, 2))
 
     def test_is_leap_year(self):
 
